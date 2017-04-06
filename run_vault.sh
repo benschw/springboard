9bfd1e2b7d35c476f9b54a6460f2cd59f675891d
Ben Schwartz <benschw@gmail.com>
2017-04-06 11:50:01 -0500

    updates to build

--- run_vault.sh
+++ run_vault.sh
@@ -3,9 +3,9 @@
 
 rm -f vault
 
-wget http://dl.fligl.io/vault -O /vault
-chmod 755 /vault
-/vault server -dev -dev-root-token-id=horde &
+wget http://dl.fligl.io/vault -O ~/vault
+chmod 755 ~/vault
+~/vault server -dev -dev-root-token-id=horde &
 sleep 3
-/vault mount transit
-/vault write -f transit/keys/my-key
+~/vault mount transit
+~/vault write -f transit/keys/my-key
