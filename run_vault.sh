e8841bf2123c08fa28fe638028710136559bea75
Ben Schwartz <benschw@gmail.com>
2017-04-06 11:47:04 -0500

    updates to build

---
+++ run_vault.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+rm -f vault
+
+wget http://dl.fligl.io/vault
+chmod 755 vault
+./vault server -dev -dev-root-token-id=horde &
+sleep 3
+./vault mount transit
+./vault write -f transit/keys/my-key
